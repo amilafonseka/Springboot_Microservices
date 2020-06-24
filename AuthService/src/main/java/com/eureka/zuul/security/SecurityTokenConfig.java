71b29aa6d112fc37a9723c63d6ac8f85b87e94f9
Amila Sembunaidelage <amila.sembunaidelage@wellcomww.com.au>
2020-06-24 16:10:04 +1000

    AuthService

---
+++ AuthService/src/main/java/com/eureka/zuul/security/SecurityTokenConfig.java
@@ -0,0 +1,39 @@
+package com.eureka.zuul.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@EnableWebSecurity // Enable security config. This annotation denotes config for spring security.
+public class SecurityTokenConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private JwtConfig jwtConfig;
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		http
+			.csrf().disable()
+				// make sure we use stateless session; session won't be used to store user's state.
+				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			.and()
+				// handle authorized attempts
+				.exceptionHandling().authenticationEntryPoint((req, rsp, e) -> rsp.sendError(HttpServletResponse.SC_UNAUTHORIZED))
+			.and()
+				//Add a filter to validate the tokens with every request
+				.addFilterAfter(new JwtTokenAuthenticationFilter(jwtConfig), UsernamePasswordAuthenticationFilter.class)
+			//authorization requests config
+			.authorizeRequests()
+				//allow ll who are accessing "auth" service
+				.antMatchers(HttpMethod.POST, jwtConfig.getUri()).permitAll()
+				//must be an admin if trying to access admin area (authentication is also required here)
+				.antMatchers("/gallery" + "/admin/**").hasRole("ADMIN")
+				//Any other request must be authenticated
+				.anyRequest().autheticated();
+		
+	}
+
+}
